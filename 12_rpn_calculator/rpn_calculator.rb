9756371edbecbe0a5074dc4dbb4303b5e2d98ea5
Danny Garcia <dannyg2620@gmail.com>
2012-12-04 17:27:11 -0500

    Update 12_rpn_calculator/rpn_calculator.rb

--- 12_rpn_calculator/rpn_calculator.rb
+++ 12_rpn_calculator/rpn_calculator.rb
@@ -2,73 +2,72 @@
 
 class RPNCalculator
 
-	attr_accessor :stack
+ attr_accessor :stack
 
-	def initialize(stack = [])
-		@stack = stack
-	end
+ def initialize(stack = [])
+  @stack = stack
+ end
 
-	def value
-		stack[stack.length - 1]
-	end
+ def value
+  stack[stack.length - 1]
+ end
 
-	def push(value)
-		stack << value.to_f
-	end
+ def push(value)
+  stack << value.to_f
+ end
 
-	def plus
-		stack << (pop + pop)
-	end
+ def plus
+  stack << (pop + pop)
+ end
 
-	def minus
-		stack << (pop - pop)
-	end
+ def minus
+  stack << (pop - pop)
+ end
 
-	def times
-		stack << (pop * pop)
-	end
+ def times
+  stack << (pop * pop)
+ end
 
-	def divide
-		stack << (pop / pop)
-	end
+ def divide
+  stack << (pop / pop)
+ end
 
-	def pop
-		num = stack.pop
-		if num.nil? 
-			raise "calculator is empty"
-		end
-		num
-	end
+ def pop
+  num = stack.pop
+  if num.nil? 
+   raise "calculator is empty"
+  end
+  num
+ end
 
-	def tokens(n)
-		token = []
-		n.split.collect do |x|
-			if x =~ /[- + \/ *]/
-				token << :"#{x}"
-			else
-				token << x.to_f
-			end
-		end
-		token
-	end
+ def tokens(n)
+  token = []
+  n.split.collect do |x|
+   if x =~ /[- + \/ *]/
+    token << :"#{x}"
+   else
+    token << x.to_f
+   end
+  end
+  token
+ end
 
-	def evaluate(string)
+  def evaluate(string)
 
-		tokens(string).each do |x|
-			if x == :+
-				plus
-			elsif x == :-
-				minus
-			elsif x == :/
-				divide
-			elsif x == :*
-				times
-			else
-				push(x)
-			end
-		end
-		value
-	end
-
+  tokens(string).each do |x|
+   if x == :+
+    plus
+   elsif x == :-
+    minus
+   elsif x == :/
+    divide
+   elsif x == :*
+    times
+   else
+    push(x)
+   end
+  end
+ value
+ end
 end 
 
