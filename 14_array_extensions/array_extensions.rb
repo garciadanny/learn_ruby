6774661c478c26dd09de67d47ee07cb362b7390b
Danny Garcia <dannyg2620@gmail.com>
2012-12-04 17:28:43 -0500

    Update 14_array_extensions/array_extensions.rb

--- 14_array_extensions/array_extensions.rb
+++ 14_array_extensions/array_extensions.rb
@@ -3,26 +3,25 @@
 
 class Array
 
-  def sum
-    output = 0
-    self.each do |n|
-      output += n 
-    end
-    output
-  end
+ def sum
+  output = 0
+   self.each do |n|
+    output += n 
+   end
+   output
+ end
 
-  def square
-  	output = []
-  	self.each do |n|
-  		output << (n**2)
-  	end
-  	output
+ def square
+  output = []
+  self.each do |n|
+   output << (n**2)
   end
+   output
+ end
 
-  def square!
-  	self.collect! do |n|
-  		(n**2)
-  	end
+ def square!
+  self.collect! do |n|
+  	(n**2)
   end
-
+ end
 end
